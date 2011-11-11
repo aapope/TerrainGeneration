4a4e038fb679d707358d428ea71519dcacb1ee19
Andrew Pope <microborg@gmail.com>
2011-11-10 21:57:19 -0700

    Minimal grass class

---
+++ Grass.py
@@ -0,0 +1,13 @@
+
+class Grass:
+    
+    #create a number of grass objects either for a map (in 
+    #which case, send it the areas it can go in) or just
+    #in general. Probably tie the render
+    def __init__(self):
+        pass
+    
+    def create_grass_list(self):
+        
+
+    
